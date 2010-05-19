22d772b345e4395c378d7162f17fd6d441decc8c
David Bellot <david.bellot@gmail.com>
2010-05-19 10:36:53 +0000

    after, testing, finally, the doxygen documentation will be included in the code.
    svn path=/branches/ublas-doxygen/; revision=62095

--- matrix.hpp
+++ matrix.hpp
@@ -3276,6 +3276,19 @@ namespace boost { namespace numeric { namespace ublas {
 
 
     // Array based matrix class
+    /// \brief An array based matrix class which size is defined at type specification or object instanciation
+    /// An array based matrix class which size is defined at type specification or object instanciation.
+    /// This matrix is directly based on a predefinec C-style arry of data, thus providing the fastest
+    /// implementation possible. The constraint is that dimensions of the matrix must be specified at 
+    /// the instanciation or the type specification. For instance, 
+    /// \code typedef c_matrix<double,4,4> my_4by4_matrix\endcode defines a 4x4 double-precision matrix.
+    /// You can also instantiate it directly with \code c_matrix<int,8,5> my_fast_matrix\endcode.
+    /// This will make a 8 by 5 integer matrix. The price to pay for this speed is that you cannot resize it 
+    /// to a size larger than the one defined in the template parameters. In the previous example, a size of
+    /// 4 by 5 or 3 by 2 is acceptable, but a new size of 9 by 5 or even 10 by 10 will raise a bad_size() exception.
+    /// \tparam T
+    /// \tparam N
+    /// \tparam M
     template<class T, std::size_t N, std::size_t M>
     class c_matrix:
         public matrix_container<c_matrix<T, N, M> > {
