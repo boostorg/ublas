08e64804bd02af15d834fdf7b8e4333ed73deb9c
Joaquim Duran <jduran.gm@gmail.com>
2014-11-06 00:51:35 +0100

    The matrices and vectors compared should have the same size.

--- test/common/testhelper.hpp
+++ test/common/testhelper.hpp
@@ -54,8 +54,13 @@ std::pair<unsigned, unsigned> getResults() {
 template < class M1, class M2 >
 bool compare( const boost::numeric::ublas::matrix_expression<M1> & m1, 
               const boost::numeric::ublas::matrix_expression<M2> & m2 ) {
-  size_t size1 = (std::min)(m1().size1(), m2().size1());
-  size_t size2 = (std::min)(m1().size2(), m2().size2());
+  if ((m1().size1() != m2().size1()) ||
+      (m1().size2() != m2().size2())) {
+    return false;
+  }
+
+  size_t size1 = m1().size1();
+  size_t size2 = m1().size2();
   for (size_t i=0; i < size1; ++i) {
     for (size_t j=0; j < size2; ++j) {
       if ( m1()(i,j) != m2()(i,j) ) return false;
@@ -67,7 +72,11 @@ bool compare( const boost::numeric::ublas::matrix_expression<M1> & m1,
 template < class M1, class M2 >
 bool compare( const boost::numeric::ublas::vector_expression<M1> & m1, 
               const boost::numeric::ublas::vector_expression<M2> & m2 ) {
-  size_t size = (std::min)(m1().size(), m2().size());
+  if (m1().size() != m2().size()) {
+    return false;
+  }
+
+  size_t size = m1().size();
   for (size_t i=0; i < size; ++i) {
     if ( m1()(i) != m2()(i) ) return false;
   }
