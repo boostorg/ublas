b3dbc13bc3e576f103257bb9cbfe6c9bc92fe9f8
rajaditya-m <rajadityamukherjee.osu@outlook.com>
2015-06-18 23:28:59 +0530

    Transform Accumulations
    This is needed for the computation of Eigen Vectors

--- include/boost/numeric/ublas/hessenberg.hpp
+++ include/boost/numeric/ublas/hessenberg.hpp
@@ -10,6 +10,9 @@
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/numeric/ublas/triangular.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
+#include <iostream>
 
 namespace boost { namespace numeric { namespace ublas {
 
@@ -49,6 +52,51 @@ namespace boost { namespace numeric { namespace ublas {
 
 	}
 
+	template<class M>
+	typename M::size_type to_hessenberg(M &m, M &u0) {
+
+		typedef typename M::size_type size_type;
+		typedef typename M::value_type value_type;
+
+		M m_copy = M(m);
+
+		//At this point, we should probably check if the number of rows and columns of M are the same or not 
+		size_type num_rows = m.size1();
+		size_type num_cols = m.size2();
+		BOOST_UBLAS_CHECK(num_rows != num_cols, singular()); //Throw some kind of assertion error saying that eigen solver works only for sqaure matrices
+
+		size_type n = num_rows;
+		u0 = identity_matrix<value_type>(n);
+		for (size_type i = 0; i < n - 2; ++i) {
+			vector<value_type> x = project(column(m, i), range(i + 1, n));
+			vector<value_type> v;
+			value_type beta;
+			householder(x, v, beta);
+
+			//std::cout << i << " " << x << " " << v << "\n";
+
+			matrix<value_type> vvt = outer_prod(v, v);
+			vvt *= beta;
+			size_type n_vvt = vvt.size1();
+			matrix<value_type> imvvt = identity_matrix<value_type>(n_vvt) -vvt;
+
+			matrix<value_type> pk = identity_matrix<value_type>(n);
+			project(pk, range(i+1, n), range(i+1, n)).assign(imvvt);
+
+			matrix<value_type> t0 = prod(u0, pk);
+			u0 = t0;
+
+			matrix<value_type> t1 = prod(imvvt, project(m, range(i + 1, n), range(i, n)));
+			project(m, range(i + 1, n), range(i, n)).assign(t1);
+			matrix<value_type> t2 = prod(project(m, range(0, n), range(i + 1, n)), imvvt);
+			project(m, range(0, n), range(i + 1, n)).assign(t2);
+
+			//@TODO: Ask mentor why uncommented lines are more accurate than commented lines... 
+			//project(m, range(i + 1, n), range(i, n)).assign(prod(imvvt, project(m, range(i + 1, n), range(i, n))));
+			//project(m, range(0, n), range(i+1, n)).assign(prod(project(m, range(0, n), range(i+1, n)),imvvt));
+
+		}
+	}
 
 }}}
 
--- include/boost/numeric/ublas/schur_decomposition.hpp
+++ include/boost/numeric/ublas/schur_decomposition.hpp
@@ -100,10 +100,147 @@ namespace boost {
 					std::cout << p << " " << q << "\n";
 				}
 			}
+
+			template<class M>
+			void schur_decomposition(M &h, M &qv) {
+
+				typedef typename M::size_type size_type;
+				typedef typename M::value_type value_type;
+
+				size_type n = h.size1();
+				size_type p = n;
+				while (p > size_type(2)) {
+					size_type q = p - size_type(1);
+
+					value_type s = h(q - size_type(1), q - size_type(1)) + h(p - size_type(1), p - size_type(1));
+					value_type t = h(q - size_type(1), q - size_type(1)) * h(p - size_type(1), p - size_type(1)) - h(q - size_type(1), p - size_type(1)) * h(p - size_type(1), q - size_type(1));
+					value_type x = h(size_type(0), size_type(0)) * h(size_type(0), size_type(0)) +
+						h(size_type(0), size_type(1)) * h(size_type(1), size_type(0)) -
+						s * h(size_type(0), size_type(0)) + t;
+					value_type y = h(size_type(1), size_type(0)) * (h(size_type(0), size_type(0)) + h(size_type(1), size_type(1)) - s);
+					value_type z = h(size_type(1), size_type(0)) * h(size_type(2), size_type(1));
+
+					//Ask mentor if we can assume that size_type will support signed type (otherwise we need a different mechanism 
+					// On second thought lets make no such assumptions 
+					for (size_type k = size_type(0); k <= p - size_type(3); k++) {
+						vector<value_type> tx(3);
+						tx(0) = x; tx(1) = y; tx(2) = z;
+						vector<value_type> v;
+						value_type beta;
+						householder<vector<value_type> >(tx, v, beta);
+
+						size_type r = (std::max)(size_type(1), k);
+
+						matrix<value_type> vvt = outer_prod(v, v);
+						vvt *= beta;
+						size_type n_vvt = vvt.size1();
+						matrix<value_type> imvvt = identity_matrix<value_type>(n_vvt) -vvt;
+
+						matrix<value_type> t1 = prod(imvvt, project(h, range(k, k + size_type(3)), range(r - size_type(1), n)));
+						project(h, range(k, k + size_type(3)), range(r - size_type(1), n)).assign(t1);
+
+						r = (std::min)(k + size_type(4), p);
+
+						matrix<value_type> t2 = prod(project(h, range(0, r), range(k, k + size_type(3))), imvvt);
+						project(h, range(0, r), range(k, k + size_type(3))).assign(t2);
+
+						matrix<value_type> t3 = prod(project(qv, range(0, n), range(k, k + size_type(3))), imvvt);
+						project(qv, range(0, n), range(k, k + size_type(3))).assign(t3);
+
+						x = h(k + size_type(1), k);
+						y = h(k + size_type(2), k);
+
+						if (k < (p - size_type(3))) {
+							z = h(k + size_type(3), k);
+						}
+
+					}
+
+					vector<value_type> tx(2);
+					tx(0) = x; tx(1) = y;
+					vector<value_type> v;
+					value_type beta;
+					householder<vector<value_type> >(tx, v, beta);
+
+					matrix<value_type> vvt = outer_prod(v, v);
+					vvt *= beta;
+					size_type n_vvt = vvt.size1();
+					matrix<value_type> imvvt = identity_matrix<value_type>(n_vvt) -vvt;
+
+					matrix<value_type> t1 = prod(imvvt, project(h, range(q - size_type(1), p), range(p - size_type(3), n)));
+					project(h, range(q - size_type(1), p), range(p - size_type(3), n)).assign(t1);
+
+					matrix<value_type> t2 = prod(project(h, range(0, p), range(p - size_type(2), p)), imvvt);
+					project(h, range(0, p), range(p - size_type(2), p)).assign(t2);
+
+					matrix<value_type> t3 = prod(project(qv, range(0,n), range(p - size_type(2),p)), imvvt);
+					project(qv, range(0, p), range(p - size_type(2), p)).assign(t3);
+
+					if ((std::abs)(h(p - size_type(1), q - size_type(1))) < (EPSA)*((std::abs)(h(p - size_type(1), p - size_type(1))) + (std::abs)(h(q - size_type(1), q - size_type(1))))) {
+						h(p - size_type(1), q - size_type(1)) = value_type(0);
+						p = p - size_type(1);
+						q = p - size_type(1);
+					}
+					else if ((std::abs)(h(p - size_type(2), q - size_type(2))) < (EPSA)*((std::abs)(h(q - size_type(2), q - size_type(2))) + (std::abs)(h(q - size_type(1), q - size_type(1))))) {
+						h(p - size_type(2), q - size_type(2)) = value_type(0);
+						p = p - size_type(2);
+						q = p - size_type(1);
+					}
+					//std::cout << p << " " << q << "\n";
+				}
+			}
+
 		
-		
-		
-		
+			template<class M> 
+			void inverse_iterations(M &h, M &u0, M &v) {
+				typedef typename M::size_type size_type;
+				typedef typename M::value_type value_type;
+
+				size_type n = h.size1();
+
+				v = identity_matrix <value_type>(n); 
+				matrix<value_type> in = identity_matrix<value_type>(n);
+				//This is the main loop
+				for (size_type i = size_type(0); i < n; i++) {
+					value_type mu = h(i, i);
+					matrix<value_type> mu_i = mu * in;
+					matrix<value_type> mat_lhs = h - mu_i;
+					size_type max_iters = size_type(1000); //This should be adjustable
+					vector<value_type> q = column(v, i);
+					vector<value_type> z;
+					int counter = 0;
+					//This is the iteration loop
+					bool convergence_reached = false;
+					while (!convergence_reached) {
+						//Start solve of mat_lhs * z = q via column oriented back-substitution 
+						//vector<value_type> q_copy = q;
+						for (size_type j = n; j >= size_type(2); j--) {
+							q(j - size_type(1)) = q(j - size_type(1)) / mat_lhs(j - size_type(1), j - size_type(1));
+							vector<value_type> u_ranged = project(column(mat_lhs, j - size_type(1)), range(size_type(0), j - size_type(1)));
+							u_ranged *= q(j - size_type(1));
+							vector<value_type> b_ranged = project(q, range(size_type(0), j - size_type(1)));
+							b_ranged -= u_ranged;
+							project(q, range(size_type(0), j - size_type(1))).assign(b_ranged);
+						}
+						q(size_type(0)) = q(size_type(0)) / mat_lhs(size_type(0), size_type(0)); 
+					  
+						//Normalize the q
+						value_type q_norm = norm_2(q);
+						//q = q / q_norm;
+
+						counter++;
+						std::cout << counter  << " " << q << "\n";
+						if (counter==10)
+							convergence_reached = true;
+					}
+
+					column(v, i) = q;
+
+
+				}
+
+
+			}
 }}}
 
 
