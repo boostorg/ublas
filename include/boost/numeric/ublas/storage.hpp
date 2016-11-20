76ee1d5aa0e47b01ed4ad7170d69d94c095f3099
Michael Krasnyk <michael.krasnyk@gmail.com>
2016-11-20 02:55:55 +0100

    Added missing array_wrapper.hpp
    related commit 6b33d1cd4e11daaf97612561ecd9d4848843897c
    in https://github.com/boostorg/serialization

--- include/boost/numeric/ublas/storage.hpp
+++ include/boost/numeric/ublas/storage.hpp
@@ -19,6 +19,7 @@
 #endif
 
 #include <boost/serialization/array.hpp>
+#include <boost/serialization/array_wrapper.hpp>
 #include <boost/serialization/collection_size_type.hpp>
 #include <boost/serialization/nvp.hpp>
 
