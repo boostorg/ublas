9eb5abd0252effe8e3f324f28f0a3b6ac5b85649
David Bellot <david.bellot@gmail.com>
2019-06-10 19:52:26 +1000

    moving ublas to a new place

---
+++ include/boost/ublas/ublas.hpp
@@ -0,0 +1,16 @@
+/// @file ublas.hpp is the main and only include file to use for Boost.uBlas users
+//
+//  Copyright (c) 2019 David Bellot
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef _BOOST_UBLAS
+#define _BOOST_UBLAS
+
+#include <boost/ublas/ublas.hpp>
+
+
+#endif
