dee775a1f499e14aa28f1659a38c563f3112d0fa
Ashar <coder3101@users.noreply.github.com>
2020-06-29 20:29:47 +0530

    Update report_coverage.sh

--- .ci/report_coverage.sh
+++ .ci/report_coverage.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -e
 
+sudo apt-get install lcov
 lcov --rc lcov_branch_coverage=1 --directory $BOOST_ROOT --capture --output-file coverage.info 
 
 # Remove all unwanted coverages libs. 
