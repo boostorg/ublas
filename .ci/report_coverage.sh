5cbe2b6c4f4fe19e802986c7289111d3e6a02f6c
Ashar <coder3101@users.noreply.github.com>
2020-06-29 14:36:04 +0530

    Remove stdc++17 requirement for reports.
    We can manually call the script only when the standard is non-c++11

--- .ci/report_coverage.sh
+++ .ci/report_coverage.sh
@@ -1,19 +1,9 @@
 #! /bin/bash -e
 
-# Only collect and upload coverage reports from C++17 Builds
-# This is required because Tensor Extension only runs with C++17
+lcov --rc lcov_branch_coverage=1 --directory $BOOST_ROOT --capture --output-file coverage.info 
 
-if [ $CXXSTD == '17' ] 
-    then 
-    # Capture all the coverge reports
-    lcov --rc lcov_branch_coverage=1 --directory $BOOST_ROOT --capture --output-file coverage.info 
+# Remove all unwanted coverages libs. 
+# Boost.uBLAS depends uses many internal boost libs, we don't want them to be in coverage.
     
-    # Remove all unwanted coverages libs. 
-    # Boost.uBLAS depends uses many internal boost libs, we don't want them to be in coverage.
-    
-    lcov --rc lcov_branch_coverage=1 --extract coverage.info "*/boost/numeric/ublas/*" "*/libs/numeric/ublas/*" --output-file coverage.info
-    
-    lcov --rc lcov_branch_coverage=1 --list coverage.info
-else
-    echo "Skipping Coverage Reporting as CXXSTD is 11"
-fi
+lcov --rc lcov_branch_coverage=1 --extract coverage.info "*/boost/numeric/ublas/*" "*/libs/numeric/ublas/*" --output-file coverage.info
+lcov --rc lcov_branch_coverage=1 --list coverage.info
