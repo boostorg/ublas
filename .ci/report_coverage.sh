fb4e37338a79a7eab672f2dbd65aef3d236f706f
Ashar <coder3101@users.noreply.github.com>
2020-06-29 16:37:32 +0530

    Update report_coverage.sh

--- .ci/report_coverage.sh
+++ .ci/report_coverage.sh
@@ -4,5 +4,6 @@ lcov --rc lcov_branch_coverage=1 --directory $BOOST_ROOT --capture --output-file
 
 # Remove all unwanted coverages libs. 
 # Boost.uBLAS depends uses many internal boost libs, we don't want them to be in coverage. 
+
 lcov --rc lcov_branch_coverage=1 --extract coverage.info "*/boost/numeric/ublas/*" "*/libs/numeric/ublas/*" --output-file coverage.info
 lcov --rc lcov_branch_coverage=1 --list coverage.info
