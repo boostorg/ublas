6a26e0f73ae2672c9a09018a71a57a66abaf5c8e
Amit Singh <amitsingh19975@gmail.com>
2020-07-11 23:23:00 +0530

    fixing examples

--- examples/tensor/fixed_rank_tensor.cpp
+++ examples/tensor/fixed_rank_tensor.cpp
@@ -50,16 +50,16 @@ int main() {
      * Value Type: float
      * Extents Type: dynamic_extents<5>{1,2,2,3,4}
     **/
-    auto t2 = ub::fixed_rank_tensor( ub::dynamic_extents<5>{1,2,2,3,4}, 5.f );
+    auto t2 = ub::fixed_rank_tensor<float,5>( ub::dynamic_extents<5>{1,2,2,3,4}, 5.f );
 
     /*--------------------------Expressions------------------------------*/
     
     using f_tensor = ub::fixed_rank_tensor<float,5>;
     using s_tensor = ub::static_tensor<float, ub::static_extents<1,2,3,4,5> >;
     
-    auto t3 = ub::fixed_rank_tensor( ub::dynamic_extents<5>{1,2,3,4,5}, 5.f );
-    auto st1 = s_tensor{ 5.f };
-    auto dt1 = ub::dynamic_tensor( ub::dynamic_extents<>{1,2,3,4,5}, 5.f );
+    auto t3 = ub::fixed_rank_tensor<float,5>( ub::dynamic_extents<5>{1,2,3,4,5}, 5.f );
+    auto st1 = s_tensor{ ub::static_extents<1,2,3,4,5>{}, 5.f };
+    auto dt1 = ub::dynamic_tensor<float>( ub::dynamic_extents<>{1,2,3,4,5}, 5.f );
 
     f_tensor exp1 = t3 + t3 * 2 + t3*t3;
     
@@ -82,7 +82,7 @@ int main() {
     ub::fixed_rank_tensor<float,5> t4 = st1;
 
     // Assigning dynamic_tensor into fixed_rank_tensor
-    ub::dynamic_tensor t5 = dt1;
+    ub::dynamic_tensor<float> t5 = dt1;
 
     std::cout<<"( t4 == t3 ) => "<<( t4 == t3 )<<std::endl;
     
